9518cec80ab2d85bd8f2df79dc0d519dfc46a223
Daniel Domínguez Gutiérrez <alu0101043845@ull.edu.es>
2019-02-24 18:48:08 +0000

    Practica 2

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -10,7 +10,15 @@ module.exports = rdf => {
 	book.title = $('dcterms\\:title').text();
 	book.authors = $('pgterms\\:agent pgterms\\:name').toArray().map(elem => $(elem).text());
 	book.subjects = $('[rdf\\:resource$="/LCSH"]').parent().find('rdf\\:value').toArray().map(elem => $(elem).text());
-	book.lcc = $('[rdf\\:resource$="/LCC"]').parent().find('rdf\\:Description').text();
+	book.lcc = $('[rdf\\:resource$="/LCC"]').parent().find('rdf\\:value').text();
+
+	var download = {};
+	download.link = $('dcterms\\:hasFormat pgterms\\:file').toArray().map(elem => $(elem).attr('rdf:about'));
+	download.format =  $('dcterms\\:hasFormat pgterms\\:file dcterms\\:format rdf\\:description rdf\\:value').toArray().map(elem => $(elem).text());
+
+	book.downloads = download;
+
+	
 
 	return book;
 };
--- databases/test/parse-rdf-test.js
+++ databases/test/parse-rdf-test.js
@@ -17,6 +17,8 @@ describe('parseRDF', () => {
 		expect(book).to.have.a.property('title','The Art of War');
 		expect(book).to.have.a.property('authors').that.is.an('array').with.lengthOf(2).and.contains('Sunzi, active 6th century B.C.').and.contains('Giles, Lionel');
 		expect(book).to.have.a.property('subjects').that.is.an('array').with.lengthOf(2).and.contains('Military art and science -- Early works to 1800').and.contains('War -- Early works to 1800');
-		expect(book).to.have.a.property('lcc').that.is.an('string').with.lengthOf().isAtLeast(1).and.startsWith().uppercase().excludingEvery(['I','O','W','X','Y']);
+		expect(book).to.have.a.property('lcc').that.is.an('string').and.to.have.a.lengthOf.at.least(1).and.to.match(/^(A|B|C|D|E|F|G|H|J|K|L|M|N|P|Q|R|S|T|U|V|Z)/);
+		expect(book).to.have.a.property('downloads').that.has.a.property('link').that.is.an('array').with.lengthOf(10).and.contains('http://www.gutenberg.org/ebooks/132.txt.utf-8');
+		expect(book).to.have.a.property('downloads').that.has.a.property('format').that.is.an('array').with.lengthOf(11).that.contains('text/plain');
 	});
 });
